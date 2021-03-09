37e21e3547166e37eee05587b3521635deee0e2f
MahmoudSiliman <modysiliman75@gmail.com>
2021-03-10 01:35:32 +0200

    app routing pages

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="main_wraper">
+    <router-view></router-view>
+  </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,49 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router';
 
-const router = createRouter(
-  {
-    history: createWebHistory(),
-    routes: [
-      {path: '/', redirect: '/coaches'},
-      {path: '/coaches', component: null},
-      {
-        path: '/coaches/:id', 
-        component: null, 
-        children: [
-          {path: 'contact', component: null}
-        ]
-      },
-      {path: '/register', component: null},
-      {path: '/requests', component: null},
-      {path: '/:notFound(.*)', component: null}
-    ],
-  }
-);
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue"
+
+import NotFound from "./pages/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches'
+    },
+    {
+      path: '/coaches',
+      component: CoachesList
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetails,
+      children: [{
+        path: 'contact',
+        component: ContactCoach
+      }]
+    },
+    {
+      path: '/register',
+      component: CoachRegistration
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
+  ]
+});
 
 export default router;
