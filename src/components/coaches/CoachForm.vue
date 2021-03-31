1ad8b6d23038b81f3cf06673b0a202cda5f3de5e
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-03-31 14:21:30 +0200

    add coach form comp

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <form>
+    <b-row>
+      <b-col cols="12" md="6" class="mb-4">
+        <div class="form_controle">
+          <input type="text" placeholder="First Name" id="first_name">
+          <icon name="pencil-alt" size="18px" color="#777"></icon>
+        </div>
+      </b-col>
+
+      <b-col cols="12" md="6" class="mb-4">
+        <div class="form_controle">
+          <input type="text" placeholder="Last Name" id="last_name">
+          <icon name="pencil-alt" size="18px" color="#777"></icon>
+        </div>
+      </b-col>
+
+      <b-col cols="12" md="6" class="mb-4">
+        <div class="form_controle">
+          <input type="number" min="1" placeholder="Hourly Rate" id="houyrly_rate">
+          <icon name="dollar-sign" size="18px" color="#777"></icon>
+        </div>
+      </b-col>
+
+      <b-col cols="12" class="mb-4">
+        <div class="form_controle d-block">
+          <h4 class="text-center"> Areas Of Experties </h4>
+
+          <section class="d-flex">
+            <div class="d-flex">
+              <input type="checkbox" id="frontend" value="frontend">
+              <label for="frontend"> Front End </label> 
+            </div>
+
+            <div class="d-flex">
+              <input type="checkbox" id="backend" value="backend">
+              <label for="backend"> Back End </label> 
+            </div>
+
+            <div class="d-flex">
+              <input type="checkbox" id="carrer" value="carrer">
+              <label for="carrer"> Carrer </label> 
+            </div>
+          </section>
+        </div>
+      </b-col>
+
+      <b-col cols="12" class="mb-4">
+        <div class="form_controle">
+          <textarea type="text" placeholder="Descreptoin" rows="5" id="descreptoin"></textarea>
+          <icon name="bookmark" size="18px" color="#777"></icon>
+        </div>
+      </b-col>
+    </b-row>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+.form_controle {
+  box-shadow: 0 0 10px #555;
+  border-radius: 15px;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+
+  input,
+  textarea {
+    width: 100%;
+    border: none;
+    &:focus {
+      outline: none;
+      border: none;
+      box-shadow: none;
+    }
+  }
+}
+</style>
--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1> Registeration </h1>
+  <div class="registeration"> 
+    <b-container>
+      <b-row class="justify-content-center my-5">
+        <b-col cols="9">
+          <base-card> 
+            <h2 class="mb-4"> Register As A Coach Now! </h2>
+            <coach-form></coach-form>
+          </base-card>
+        </b-col>
+      </b-row>
+    </b-container>
+
+  </div>
 </template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+
+export default {
+  components: {
+    CoachForm,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.registeration {
+  min-height: calc( 100vh - 103px );
+
+  .main_card {
+    h2 {
+      text-align: center;
+      color: #ff7b54;
+    }
+  }
+}
+</style>
