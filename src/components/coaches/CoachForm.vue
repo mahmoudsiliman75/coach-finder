a9cb32b83bd455c1f31c65e406b13961fb10d64b
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-04-10 11:52:54 +0200

    add validtion to registeration form

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -2,43 +2,43 @@
   <form @submit.prevent="submitForm">
     <b-row>
       <b-col cols="12" md="6" class="mb-4">
-        <div class="form_controle">
-          <input type="text" placeholder="First Name" id="first_name" v-model.trim="firstName">
+        <div class="form_controle" :class= '!firstName.isValid ?  "invalid" : ""'>
+          <input type="text" placeholder="First Name" id="first_name" v-model.trim="firstName.val">
           <icon name="pencil-alt" size="18px" color="#777"></icon>
         </div>
       </b-col>
 
       <b-col cols="12" md="6" class="mb-4">
-        <div class="form_controle">
-          <input type="text" placeholder="Last Name" id="last_name" v-model.trim="lastName">
+        <div class="form_controle" :class= '!lastName.isValid ?  "invalid" : ""'>
+          <input type="text" placeholder="Last Name" id="last_name" v-model.trim="lastName.val">
           <icon name="pencil-alt" size="18px" color="#777"></icon>
         </div>
       </b-col>
 
       <b-col cols="12" md="6" class="mb-4">
-        <div class="form_controle">
-          <input type="number" min="1" placeholder="Hourly Rate" id="houyrly_rate" v-model.trim="rate">
+        <div class="form_controle" :class= '!rate.isValid ?  "invalid" : ""'>
+          <input type="number" min="1" placeholder="Hourly Rate" id="houyrly_rate" v-model.trim="rate.val">
           <icon name="dollar-sign" size="18px" color="#777"></icon>
         </div>
       </b-col>
 
       <b-col cols="12" class="mb-4">
-        <div class="form_controle d-block">
+        <div class="form_controle d-block" :class= '!areas.isValid ?  "invalid" : ""'>
           <h4 class="text-center mb-3"> Areas Of Experties </h4>
 
           <section class="d-flex justify-content-around">
             <div>
-              <input type="checkbox" id="frontend" value="frontend" v-model.number="areas">
+              <input type="checkbox" id="frontend" value="frontend" v-model.number="areas.val">
               <label for="frontend"> Front End </label> 
             </div>
 
             <div>
-              <input type="checkbox" id="backend" value="backend" v-model.number="areas">
+              <input type="checkbox" id="backend" value="backend" v-model.number="areas.val">
               <label for="backend"> Back End </label> 
             </div>
 
             <div>
-              <input type="checkbox" id="carrer" value="carrer" v-model.number="areas">
+              <input type="checkbox" id="carrer" value="carrer" v-model.number="areas.val">
               <label for="carrer"> Carrer </label> 
             </div>
 
@@ -47,8 +47,8 @@
       </b-col>
 
       <b-col cols="12" class="mb-4">
-        <div class="form_controle">
-          <textarea type="text" placeholder="Descreptoin" rows="5" id="descreptoin" v-model.trim="descripion"></textarea>
+        <div class="form_controle" :class= '!descripion.isValid ?  "invalid" : ""'>
+          <textarea type="text" placeholder="Descreptoin" rows="5" id="descreptoin" v-model.trim="descripion.val"></textarea>
           <icon name="bookmark" size="18px" color="#777"></icon>
         </div>
       </b-col>
@@ -66,52 +66,117 @@ export default {
 
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      rate: null,
-      descripion: '',
-      areas: [],
+      formIsValid: true,
+      firstName: {
+        val: '',
+        isValid: true,
+      },
+      lastName: {
+        val: '',
+        isValid: true,
+      },
+      rate: {
+        val: null,
+        isValid: true,
+      },
+      descripion: {
+        val: '',
+        isValid: true,
+      },
+      areas: {
+        val: [],
+        isValid: true,
+      },
     }
   },
 
   methods: {
-    showAlert() {
+    showAlert(type, message) {
       this.$swal({
-        icon: 'success',
-        title: 'Registered Successfuly',
+        icon: type,
+        title: message,
       });
     },
 
-    submitForm() {
-      const formData = {
-        first: this.firstName,
-        last: this.lastName,
-        rate: this.rate,
-        desc: this.descripion,
-        areas: this.areas,
+    validateForm() {
+      this.formIsValid = true;
+
+      if(this.firstName.val == '') {
+        this.firstName.isValid = false;
+        this.formIsValid = false;
+        this.showAlert('error', "First Name Can't Be Empty");
       }
 
-      this.firstName = '';
-      this.lastName = '';
-      this.rate = null;
-      this.descripion = '';
-      this.areas = [];
+      if(this.lastName.val == '') {
+        this.lastName.isValid = false;
+        this.formIsValid = false;
+        this.showAlert('error', "Last Name Can't Be Empty");
+      }
+
+      if(this.descripion.val == '') {
+        this.descripion.isValid = false;
+        this.formIsValid = false;
+        this.showAlert('error', "Description Can't Be Empty");
+      }
+
+      if(!this.rate.val) {
+        this.rate.isValid = false;
+        this.formIsValid = false;
+        this.showAlert('error', "Hourly Rate Can't Be Empty");
+      }
+
+      if(this.areas.val.length == 0) {
+        this.areas.isValid = false;
+        this.formIsValid = false;
+        this.showAlert('error', "At Least One Area Of Experties Must Be Selected");
+      }
+    },
+
+    submitForm() {
+      this.validateForm();
+
+      if(!this.formIsValid) {
+        return;
+      }
+
+      const formData = {
+        first: this.firstName.val,
+        last: this.lastName.val,
+        rate: this.rate.val,
+        desc: this.descripion.val,
+        areas: this.areas.val,
+      }
+
+      this.firstName.val = '';
+      this.lastName.val = '';
+      this.rate.val = null;
+      this.descripion.val = '';
+      this.areas.val = [];
 
       this.$emit('save-data', formData);
 
-      this.showAlert()
-    }
+      this.showAlert('success', 'Registered Successfuly');
+    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
+h4 {
+  color: #ff7b54;
+}
+
 .form_controle {
   box-shadow: 0 0 10px #555;
   border-radius: 15px;
   padding: 8px;
   display: flex;
   align-items: center;
+
+  &.invalid {
+    color: #f00;
+    border: 1px solid red;
+  }
 
   input,
   textarea {
